cc9cfd34b5e2680234ea3efd6c19f1251613543a
Professional <Professional@USER-PC>
2025-04-01 00:47:05 +0300

    Add project files.

---
+++ ShutDown/WinOFF.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ShutDown
+{
+    partial class WinOFF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WinOFF));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Location = new System.Drawing.Point(63, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 87);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Стартуем?";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(63, 324);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 87);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Галя, у нас отмена";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(87, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox2.Location = new System.Drawing.Point(34, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Часы";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(34, 106);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(75, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "минуты";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(34, 132);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(87, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label1.Location = new System.Drawing.Point(86, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // WinOFF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(279, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "WinOFF";
+            this.Text = "WinOFF";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ShutDown/WinOFF.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ShutDown
+{
+    public partial class WinOFF : Form
+    {
+        private Timer countdownTimer;
+        private int remainingTime;
+        public WinOFF()
+        {
+            InitializeComponent();
+            countdownTimer = new Timer();
+            countdownTimer.Interval = 1000; 
+            countdownTimer.Tick += CountdownTimer_Tick;
+        }
+
+        void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+                if (int.TryParse(textBox1.Text, out int hours) &&
+                    int.TryParse(textBox4.Text, out int minutes))
+                {
+                    remainingTime = hours * 3600 + minutes * 60;
+                    if (remainingTime > 0)
+                    {
+                        try
+                        {
+                        ProcessStartInfo info = new ProcessStartInfo()
+                        {
+                            FileName = @"C:\Windows\System32\shutdown.exe",
+                            Arguments = $"-s -t {remainingTime}",
+                            UseShellExecute = true,
+                            CreateNoWindow = true,
+                            WindowStyle = ProcessWindowStyle.Hidden
+                        };
+
+
+                        Process.Start(info);
+                            MessageBox.Show($"Выключение через {hours} ч. {minutes} мин.");
+                            countdownTimer.Start();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Ошибка: {ex.Message}");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Введите корректные данные");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Введите корректные данные");
+                }
+            
+
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo info = new ProcessStartInfo()
+            {
+                FileName = @"C:\Windows\System32\shutdown.exe",
+                Arguments = "-a",
+                UseShellExecute = true,
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+
+            Process.Start(info);
+            countdownTimer.Stop();
+            label1.Text = "Таймер остановлен";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = $"Осталось: {remainingTime / 3600:D2}:{(remainingTime % 3600) / 60:D2}:{remainingTime % 60:D2}";
+
+        }
+        private void CountdownTimer_Tick(object sender, EventArgs e)
+        {
+            if (remainingTime > 0)
+            {
+                remainingTime--;
+                label1.Text = $"Осталось: {remainingTime / 3600:D2}:{(remainingTime % 3600) / 60:D2}:{remainingTime % 60:D2}";
+            }
+            else
+            {
+                countdownTimer.Stop();
+            }
+        }
+
+    }
+}
